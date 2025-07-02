e0a11a9afeef077e6574e873bafc51f02f3f84ff
encorekim <rlawodus95@hanmail.net>
2025-07-02 16:52:57 +0900

    [feat] AlphabetChecker return max score with the same two inputs

--- SimilarityChecker-0702/alpha.cpp
+++ SimilarityChecker-0702/alpha.cpp
@@ -7,16 +7,22 @@ class AlphabetChecker {
  public:
   double getAlphabetScore(const string& wordA, const string& wordB) {
     if (wordA.size() == 0 && wordB.size() == 0) {
-      throw(std::invalid_argument("Invalid arguments: all zero-length strings"));
+      throw(
+          std::invalid_argument("Invalid arguments: all zero-length strings"));
     }
 
     if (wordA.size() == 0 || wordB.size() == 0) {
-      return 0;
+      return ZERO_SCORE;
+    }
+
+    if (wordA == wordB) {
+      return MAX_SCORE;
     }
 
     return 0;
   }
 
-  private:
+ private:
   double MAX_SCORE = 40;
+  double ZERO_SCORE = 0;
 };
--- SimilarityChecker-0702/alpha_test.cpp
+++ SimilarityChecker-0702/alpha_test.cpp
@@ -12,10 +12,15 @@ class AlphaFixture : public testing::Test {
   AlphabetChecker alpha;
 };
 
-TEST_F(AlphaFixture, ZeroLengthInputs) {
+TEST_F(AlphaFixture, ZeroLengthError) {
   EXPECT_THROW(alpha.getAlphabetScore("", ""), std::invalid_argument);
 }
 
 TEST_F(AlphaFixture, ZeroAndNonZero) {
   EXPECT_EQ(0, alpha.getAlphabetScore("A", ""));
 }
+
+
+TEST_F(AlphaFixture, SameInputs) {
+    EXPECT_EQ(40, alpha.getAlphabetScore("AB", "AB"));
+  }
