0e32f87ef5cefbca3ddbb5521a3a87a7e6ce47fb
encorekim <rlawodus95@hanmail.net>
2025-07-02 16:11:52 +0900

    [refactor] Change EXPECT_EQ to EXPECT_DOUBLE_EQ. Prevent floading point error

--- SimilarityChecker-0702/length_test.cpp
+++ SimilarityChecker-0702/length_test.cpp
@@ -12,20 +12,25 @@ class LengthFixture : public testing::Test {
 
 TEST_F(LengthFixture, SameLength) {
   double actual = length.getLengthScore("AAA", "BBB");
-  EXPECT_EQ(60, actual);
+  EXPECT_DOUBLE_EQ(60, actual);
 }
 
 TEST_F(LengthFixture, DoubleLength) {
   double actual = length.getLengthScore("AAA", "BBBBBB");
-  EXPECT_EQ(0, actual);
+  EXPECT_DOUBLE_EQ(0, actual);
 }
 
 TEST_F(LengthFixture, CommonLength1) {
   double actual = length.getLengthScore("AAAAAA", "BBBB");
-  EXPECT_EQ(30, actual);
+  EXPECT_DOUBLE_EQ(30, actual);
 }
 
 TEST_F(LengthFixture, CommonLength2) {
   double actual = length.getLengthScore("AAAAA", "BBBB");
-  EXPECT_EQ(45, actual);
+  EXPECT_DOUBLE_EQ(45, actual);
+}
+
+TEST_F(LengthFixture, ComplexLength) {
+  double actual = length.getLengthScore("AAAAAAAA", "BBBBBBB");
+  EXPECT_DOUBLE_EQ((double)6 / (double)7 * 60, actual);
 }
